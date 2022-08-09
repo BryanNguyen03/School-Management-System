e03f20dd6c7ab9dbfafa8c111f96f971fac01b70
Bryan <bryannguyen051012@gmail.com>
2022-08-09 14:30:51 -0400

    added student login

--- school system.py
+++ school system.py
@@ -10,20 +10,63 @@ db = mysql.connector.connect(
 myCursor = db.cursor(buffered=True)
 
 """
+Student login session
+This function takes in one string parameter representing the users username
+Students will be able to check their attendance status and download it as a text file
+"""
+def student_session(username):
+    print("\nStudent Login Successful")
+
+    username = (str(username),)
+    #query the database and store into records
+    myCursor.execute("SELECT date, username, status FROM attendance WHERE username = %s", username)
+    records = myCursor.fetchall()
+
+    while True:
+        #Student Menu
+        print("\n1. View Attendance Status")
+        print("2. Download Attendance Status")
+        print("3. Logout")
+
+        user_option = input(str("Option: "))
+
+        if user_option == "1":
+            print("Viewing Attendance Status")
+            #Print each record in records
+            for record in records:
+                print(record)
+
+        elif user_option == "2":
+            print("Downloading attendance")
+            #Write the records into a text file
+            for record in records:
+                with open("D:\Desktop\student_attendance.txt", "w") as f:
+                    f.write(str(records)+"\n")
+                f.close()
+            print("All records downloaded")
+
+        elif user_option == "3":
+            print("Logging out")
+            break
+        else:
+            print("Invalid Option")
+
+
+"""
 Professor login session
 Professors will be able to mark and check students' attendance status
 """
 def prof_session():
-    print("Professor Login Successful")
+    print("\nProfessor Login Successful")
     while True:
         #Professor Menu
-        print("\n1. Mark Student Register")
-        print("2. View Register")
+        print("\n1. Mark Student Attendance")
+        print("2. View Attendance")
         print("3. Logout")
 
         user_option = input(str("Option: "))
         if(user_option == "1"):
-            print("\nMark Student Register")
+            print("\nMark Student Attendance")
             #Query the database for the username of all students
             myCursor.execute("SELECT username FROM users WHERE accType = 'student'")
             #Fetch all the records that are returned from the query above
@@ -62,7 +105,7 @@ def prof_session():
             print("Invalid Option")
 
 def admin_session():
-    print("Admin Login Successful")
+    print("\nAdmin Login Successful")
     while True:
         #Admin Menu
         print("\n1. Register New Student")
@@ -159,6 +202,17 @@ def prof_login():
 
 def student_login():
     print("Student login\n")
+    username = input(str("Username: "))
+    password = input(str("Password: "))
+
+    #Check logind etails
+    query_values = (username, password)
+    myCursor.execute("SELECT * FROM users WHERE username = %s AND password = %s AND accType = 'student'", query_values)
+    if myCursor.rowcount < 1:
+        print("Incorrect login details")
+    else:
+        #Login is successful and we call the student_session method that takes in a string parameter representing the username
+        student_session(username)
 
 def main():
     
